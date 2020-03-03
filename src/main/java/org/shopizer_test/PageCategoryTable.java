b04ba59d09e9dfc2ac73afb0c260de8528d92b18
Sarah <sarah_bali10@yahoo.fr>
2020-03-03 11:28:02 +0100

    commit n°1  03/03/2020

--- src/main/java/org/shopizer_test/PageCategoryTable.java
+++ src/main/java/org/shopizer_test/PageCategoryTable.java
@@ -1,5 +1,39 @@
 package org.shopizer_test;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PageCategoryTable extends PageTopMenu {
 
+	
+	@FindBy (xpath="//div/ol/li[1]/a")
+	WebElement link_homePage;
+	
+	
+	@FindBy (xpath="//div/ol/li[2]/a")
+	WebElement link_tables;
+	
+	@FindBy (xpath="(//a[@href=\"/shopizer/shop/product/natural-root-console.html\"]/h3/..)[1]")
+	WebElement link_item1;
+	
+	@FindBy (xpath="(//a[@href=\"/shopizer/shop/product/asian-rosewood-console.html\"]/h3/..)[1]")
+	WebElement link_item2;
+	
+	@FindBy (xpath="(//a[@href=\"/shopizer/shop/product/edge-console.html\"]/h3/..)[1]")
+	WebElement link_item3;
+	
+	@FindBy (xpath="(//a[@href=\"/shopizer/shop/product/coffee-table-accacia.html\"]/h3/..)[1]")
+	WebElement  link_item4;
+	
+	@FindBy (xpath="(//a/h3[contains(text(),'Natural root console')]/../../h4/span)[1]")
+	WebElement price_item1;
+	
+	@FindBy (xpath="(//a/h3[contains(text(),'Asian rosewood console')]/../../h4/span)[1]")
+	WebElement price_item2;
+	
+	@FindBy (xpath="(//a/h3[contains(text(),'Edge console')]/../../h4/span)[1]")
+	WebElement price_item3;
+	
+	@FindBy (xpath="(//a/h3[contains(text(),'Coffee table Accacia')]/../../h4/span)[1]")
+	WebElement price_item4;
 }
--- src/test/java/org/shopizer_test/Test001.java
+++ src/test/java/org/shopizer_test/Test001.java
@@ -1,6 +1,6 @@
 package org.shopizer_test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
@@ -21,6 +21,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Test001 {
 
 	WebDriver driver;
+	String str_homePage="Page d'accueil";
+	String str_tables="Tables";
 
 	@Before
 	public void startup() throws ClassNotFoundException, FileNotFoundException, SQLException {
@@ -37,15 +39,31 @@ public class Test001 {
 	public void testCheckObject() throws InterruptedException {
 
 		WebDriverWait wait = new WebDriverWait(driver, 10);
-		// Step 1 : connexion à l'adresse de l'application
-
+		
+		// Step 1 : Acces à l'application
 		driver.get("http://localhost:8090/shopizer/shop");
 		
 		PageShop page_shop = PageFactory.initElements(driver, PageShop.class);
 		
+		//Step 2 : Selection de la partie Table 
 		PageCategoryTable page_table = page_shop.accessTable(driver);
 		
+		/** Presence d'une arborescence indiquant le chemin  **/
+		assertEquals(str_homePage,page_table.link_homePage.getText());
+		assertEquals(str_tables,page_table.link_tables.getText());
 		
+		
+		// Step 3 : Verification de la présence d'elements
+		assertTrue(page_table.link_item1.isDisplayed());
+		assertTrue(page_table.link_item2.isDisplayed());
+		assertTrue(page_table.link_item3.isDisplayed());
+		assertTrue(page_table.link_item4.isDisplayed());
+		
+		// Step 4 : Verification des prix 
+		System.out.println(page_table.price_item1.getText());
+		System.out.println(page_table.price_item2.getText());
+		System.out.println(page_table.price_item3.getText());
+		System.out.println(page_table.price_item4.getText());
 	}
 
 }
